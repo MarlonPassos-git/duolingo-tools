9309d13ecab848037751da06acf7663737520c33
marlon passos <marlonpassos@agenciam3.com>
2023-04-17 00:44:56 -0300

    refactor: cria funcao para deixar mais descritivo processo de mockar o document

--- src/utils/elements.test.ts
+++ src/utils/elements.test.ts
@@ -7,13 +7,16 @@ import { getIncorrectFooterContainerElement } from "./elements"
 
 describe(getIncorrectFooterContainerElement.name, () => {
   it("should return null if is in documents with not incorrect container", () => {
-    window.document.body.innerHTML = correctFooterContainer
-    console.log("saa")
+    mockDocumentWith(correctFooterContainer)
     expect(getIncorrectFooterContainerElement()).toBe(null)
   })
 
   it("should return a element if is in documents with incorrect container", () => {
-    window.document.body.innerHTML = incorrectFooterContainer
+    mockDocumentWith(incorrectFooterContainer)
     expect(getIncorrectFooterContainerElement()).not.toBe(null)
   })
+
+  function mockDocumentWith(html: string) {
+    window.document.body.innerHTML = html
+  }
 })
