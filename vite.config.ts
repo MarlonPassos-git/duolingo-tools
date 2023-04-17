c6962d10fca9acc8da5a38d406d0021219b04fd2
marlon passos <marlonpassos@agenciam3.com>
2023-04-17 00:54:01 -0300

    chore: configura eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json'
 import { defineConfig } from 'vitest/config'
+import eslint from 'vite-plugin-eslint'
 
 export default defineConfig({
   plugins: [
     crx({ manifest }),
+    eslint(),
   ],
   test: {
     globals: true,
