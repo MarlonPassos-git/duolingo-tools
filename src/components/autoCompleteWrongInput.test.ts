40a1b9d0cd5502a393bd7f5921ec6fb5a75d957b
marlon passos <marlonpassos@agenciam3.com>
2023-04-17 15:42:59 -0300

    test: garante que vamos salvar o valor no simpleStorage se caso o elemento existir

--- src/components/autoCompleteWrongInput.test.ts
+++ src/components/autoCompleteWrongInput.test.ts
@@ -1,8 +1,9 @@
 import { describe, it, expect, vi } from 'vitest'
-import { AutoCompleteWrongInput, SimpleStorage } from './autoCompleteWrongInput'
+import { AutoCompleteWrongTextArea, SimpleStorage } from './autoCompleteWrongInput'
+import { EVENT_RESULT_ERROR } from '../constants'
 
-describe(AutoCompleteWrongInput.name, () => {
 
+describe(AutoCompleteWrongTextArea.name, () => {
 
 
   it("se o erro acontecer em uma pagina que nao tem o input, nao deve fazer nada", () => {
@@ -11,7 +12,15 @@ describe(AutoCompleteWrongInput.name, () => {
     expect(storage.set).toHaveBeenCalledTimes(0)
   })
 
-  it.todo("se o error acontecer na primeira vez em uma pagina do tipo input deve salvar no local storage oque a pessoa digitou")
+  it("se o error acontecer na primeira vez em uma pagina do tipo input deve salvar no local storage oque a pessoa digitou", () => {
+    const { sut, storage, getTextAreaSpy } = makeSut()
+    sut.init()
+    getTextAreaSpy.mockReturnValue(document.createElement('textarea'))
+    const resultErrorEvent = new Event(EVENT_RESULT_ERROR)
+    document.dispatchEvent(resultErrorEvent)
+
+    expect(storage.set).toHaveBeenCalledTimes(1)
+  })
 
 
   function makeSut() {
@@ -19,9 +28,11 @@ describe(AutoCompleteWrongInput.name, () => {
       get = vi.fn()
       set = vi.fn()
     }
+    const getTextAreaSpy = vi.fn<[], HTMLTextAreaElement | null>(() => null)
+    const getQuestionIdSpy = vi.fn<[], string>(() => 'any_question_id')
     const storage = new SimpleStorageSpy()
-    const sut = new AutoCompleteWrongInput(storage)
+    const sut = new AutoCompleteWrongTextArea(storage, getTextAreaSpy, getQuestionIdSpy)
 
-    return { sut, storage }
+    return { sut, storage, getTextAreaSpy, getQuestionIdSpy }
   }
 })  
